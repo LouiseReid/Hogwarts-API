8fe50b67f8948d2d5bd591f65eae8c15ec8897e8
LouiseReid <l_reid@hotmail.co.uk>
2017-12-08 20:39:46 +0000

    slytherin and ravenclaw populated

--- public/app.js
+++ public/app.js
@@ -16,6 +16,8 @@ var requestComplete = function(){
   var students = JSON.parse(jsonString);
   populateSelectGryffindor(students);
   populateSelectHufflepuff(students);
+  populateSelectSytherin(students);
+  populateSelectRavenclaw(students);
 }
 
 
@@ -44,6 +46,33 @@ var populateSelectHufflepuff = function(students){
     }
   }
 }
+
+var populateSelectSytherin = function(students){
+  var select = document.getElementById('slytherin-select');
+  for(var i = 0; i < students.length; i++){
+    var student = students[i];
+    if(student.house === "Slytherin"){
+      var option = document.createElement('option');
+      option.innerText = student.name;
+      option.value = i;
+      select.appendChild(option)
+    }
+  }
+}
+
+var populateSelectRavenclaw= function(students){
+  var select = document.getElementById('ravenclaw-select');
+  for(var i = 0; i < students.length; i++){
+    var student = students[i];
+    if(student.house === "Ravenclaw"){
+      var option = document.createElement('option');
+      option.innerText = student.name;
+      option.value = i;
+      select.appendChild(option)
+    }
+  }
+}
+
 
 
 window.addEventListener('load', app);
