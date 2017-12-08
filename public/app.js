f55d914484bf215f4a7666d8666cae21c659b575
LouiseReid <l_reid@hotmail.co.uk>
2017-12-08 22:30:06 +0000

    selectChanged function fully working

--- public/app.js
+++ public/app.js
@@ -35,11 +35,46 @@ select.addEventListener('change', function(){
 
 var selectChanged = function(index, students){
   var studentName = document.getElementById('student-name')
+  var studentDOB = document.getElementById('student-dob')
+  var studentAncestry = document.getElementById('student-ancestry')
+  var studentStatus = document.getElementById('student-status')
+  var studentWand = document.getElementById('student-wand')
+  var wandWood = document.getElementById('wand-wood')
+  var wandCore = document.getElementById('wand-core')
+  var wandLength = document.getElementById('wand-length')
+  var studentPatronus = document.getElementById('student-patronus')
+  var studentImage = document.getElementById('student-picture')
+  var houseLogo = document.getElementById('house-logo')
   var student = students[index];
+
   studentName.innerText = student.name
+  studentDOB.innerText = "DOB: " + student.dateOfBirth
+  studentAncestry.innerText = student.ancestry
+  studentStatus.innerText = "Current student: " + student.hogwartsStudent
+  studentWand.innerText = "Wand:"
+  wandWood.innerText = "Wood: " + student.wand.wood
+  wandCore.innerText = "Core: " + student.wand.core
+  wandLength.innerText = "Length: " + student.wand.length
+  studentWand.appendChild(wandWood)
+  studentWand.appendChild(wandCore)
+  studentWand.appendChild(wandLength)
+  studentPatronus.innerText = "Patronus: " + student.patronus
+  studentImage.src = student.image
+
+  if(student.house === "Gryffindor"){
+    houseLogo.src = "gryffindor.png"
+  }
+  if(student.house === "Slytherin"){
+    houseLogo.src = "slytherin.png"
+  }
+  if(student.house === "Ravenclaw"){
+    houseLogo.src = "ravenclaw.png"
+  }
+  if(student.house === "Hufflepuff"){
+    houseLogo.src = "hufflepuff.png"
+  }
+
 }
-
-
 
 
 
